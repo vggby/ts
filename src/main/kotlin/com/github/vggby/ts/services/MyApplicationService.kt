fef2c077e364986a8a549011ab8d7e3ba1bcf9f1
GitHub Action <action@github.com>
2021-12-30 06:08:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/vggby/ts/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vggby/ts/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vggby.ts.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vggby.ts.MyBundle
 
 class MyApplicationService {
 
